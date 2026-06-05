4f2a86d76cfa270e7ffc5da34491162682bfce9d
Abel Copa <abel.copa@dharbor.com>
2026-06-04 22:09:59 -0400

    chore: add resources in output

--- infrastructure-dh/outputs.tf
+++ infrastructure-dh/outputs.tf
@@ -2,12 +2,24 @@ output "s3_bucket_name" {
   value = module.storage.bucket_name
 }
 
+output "s3_bucket_arn" {
+  value = module.storage.bucket_arn
+}
+
 output "sqs_arns" {
   value = module.messaging.sqs_arns
 }
 
+output "sqs_urls" {
+  value = module.messaging.sqs_urls
+}
+
 output "ecr_urls" {
   value = module.registry.ecr_urls
+}
+
+output "lambda_arns" {
+  value = module.compute.lambda_arns
 }
 
 output "lambda_execution_role_arn" {
